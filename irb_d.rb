7b53b7b5e6344c3fdd34e74d3cc063d171e4f95a
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2009-05-25 06:23:17 +0900

    add usage

--- irb_d.rb
+++ irb_d.rb
@@ -33,3 +33,29 @@ end
 DRb.start_service('druby://:54345', IRb.new)
 IRB.start(__FILE__)
 
+=begin
+ [terminal A] $ ruby irb_d.rb --noreadline
+ [terminal B] $ ruby irb_c.rb
+
+ [terminal B]
+ irb(main):001:0> A = 'Here'
+ => "Here"
+
+ [terminal A]
+ irb(main):001:0> A
+ => "Here"
+ irb(main):002:0> require 'thread'
+ => false
+ irb(main):003:0> $queue = Queue.new
+ => #<Queue:0x446114>
+
+ [terminal B]
+ irb(main):002:0> $queue.pop
+
+ [terminal A]
+ irb(main):004:0> $queue.push('there')
+ => #<Queue:0x446114>
+
+ [terminal B]
+ => "there'
+=end
