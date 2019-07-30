f67eedecb9e6fb37a807ba68e2c9742c392520c9
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2019-07-30 11:53:38 +0900

    use ReadlineInputMethod

--- irb_c.rb
+++ irb_c.rb
@@ -7,9 +7,11 @@ STDOUT.sync = true
 IRB.setup(__FILE__)
 
 DRb.start_service
-im = IRB::StdioInputMethod.new
+im = IRB::ReadlineInputMethod.new
 im.extend(DRbUndumped)
+om = IRB::StdioOutputMethod.new
+om.extend(DRbUndumped)
 
 ro = DRbObject.new_with_uri('druby://localhost:54345')
-th = ro.start(im, $stdout)
+th = ro.start(im, om, $stdout)
 th.join
--- irb_d.rb
+++ irb_d.rb
@@ -15,23 +15,29 @@ module IRB
     def printf(*opts)
       @stdout ? @stdout.printf(*opts) : super
     end
-  end
-end
 
-class IRb
-  def start(input_method, output)
-    irb = IRB::Irb.new(nil, input_method)
-    irb.stdout = output
-    th = Thread.new do
-      catch(:IRB_EXIT) do
-        irb.eval_input
-      end
+    def puts(*opts)
+      @stdout ? @stdout.puts(*opts) : super
     end
   end
 end
 
+class IRb
+  def start(input_method, output_method, output)
+    IRB.setup(__FILE__)
+    irb = IRB::Irb.new(nil, input_method, output_method)
+    irb.stdout = output
+    Thread.new do
+      irb.run()
+    end
+  end
+end
+
+if __FILE__ == $0
 DRb.start_service('druby://:54345', IRb.new)
-IRB.start(__FILE__)
+# IRB.start(__FILE__)
+DRb.thread.join
+end
 
 =begin
  [terminal A] $ ruby irb_d.rb --noreadline
